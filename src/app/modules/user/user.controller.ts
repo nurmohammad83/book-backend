b0e2755df0d24be348e027351f6a5ca667f78f86
Nur Mohammad <mohammadnur5480@gmail.com>
2023-07-14 06:26:16 +0600

    [Done]

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,8 +1,18 @@
 import { Request, Response } from 'express';
 import catchAsync from '../../../shared/catchAsync';
+import sendResponse from '../../../shared/sendRespons';
+import httpStatus from 'http-status';
+import { UserService } from './user.service';
 
 const createUser = catchAsync(async (req: Request, res: Response) => {
-  console.log(req, res);
+  const { ...userData } = req.body;
+  const result = await UserService.createUser(userData);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'User created successfully!',
+    data: result,
+  });
 });
 
 export const UserController = {
